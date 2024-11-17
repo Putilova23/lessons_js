c08b96a323077baab18a2b007dc948b8d59da576
Putilova23 <Alenka1118@icloud.com>
2024-11-17 15:59:59 +0700

    логика с кубиками

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,17 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 6) + 1 // your code
+let dice2 = Math.floor(Math.random() * 6) + 1 // your code
+
+let isWinningDouble = dice1 === dice2 && dice1 + dice2 > 6
+
+if (isWinningDouble) {
+  console.log('Выигрышный дубль!')
+} else {
+  console.log('Не выигрышный дубль.')
+}
+
 
 /*
 console.log('Первый бросок: ' + dice1)
