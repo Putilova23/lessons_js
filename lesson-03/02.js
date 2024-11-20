438345cb4c6f92a478cfd67181cb6579872e071d
Putilova23 <Alenka1118@icloud.com>
2024-11-20 20:28:37 +0700

    логика 2,3

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,7 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(number) {
+  const sum = number % 2
+  return sum === 0
+}
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,14 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(firstNumber, secondNumber, thirdNumber) {
+  let bigNumber 
+  if (firstNumber >= secondNumber){
+    bigNumber = firstNumber 
+  } else { 
+    bigNumber = secondNumber
+  }
+  if (thirdNumber >= bigNumber) {
+    bigNumber = thirdNumber
+  } 
+  return bigNumber
+}
