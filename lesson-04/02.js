764e3736a5c6ccee27f47ab2dab45b053e66d396
Putilova23 <Alenka1118@icloud.com>
2024-11-29 15:16:39 +0700

    логика

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,4 +15,15 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findUniqueElements() {}
+findUniqueElements([1, 2, 3, 2, 1, 4])
+
+function findUniqueElements(array) {
+  const uniqueElements = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!includesElement(uniqueElements, array[i])) {
+        uniqueElements.push(array[i]);
+    }
+}
+return uniqueElements;
+}
+
