7a43a1b65324acbc9c10efc745f381aa8e5af017
Putilova23 <Alenka1118@icloud.com>
2024-11-29 19:00:16 +0700

    kjubrf

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,15 +17,15 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-findCommonElements([1, 2, 3], [2, 3, 4])
+console.log( findCommonElements([1, 2, 3], [2, 3, 4]));
 
-function findCommonElements(array1, array2) {
-  const uniqueElements = [];
-  for (let i = 0; i < array1.length; i++) 
-  for (let i = 0; i < array2.length; i++){
-    if (array1[i].includes(array2[i])) {
-        uniqueElements.push(array1[i]);
-    }
+
+for (let i = 0; i < array1.length; i++) {
+  if (includesElement(array2, array1[i])) {
+      commonElements.push(array1[i]);
+  }
 }
-return uniqueElements(i);
-}
+return commonElements;
+  
+
+
