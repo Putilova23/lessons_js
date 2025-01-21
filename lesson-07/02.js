28c6acf40eac3e4f81fc9431010b3fbf0f9a0a92
Alena <Alena1118@icloud.com>
2025-01-21 17:01:32 +0700

    фикс

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -4,21 +4,31 @@
  */
 
 
+/**
+ * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
+ * Если строка является числом, функция должна возвращать true, в противном случае - false.
+ */
+
 function isNumeric(str) {
-  function isNumeric(str) {
-    if (str === null || typeof str !== 'string') {
-        return false;
-    }
-    if (str.length === 0) {
-        return false;
-    }
-    return !isNaN(str) && !isNaN(parseFloat(str));
-}
-  // your code
+  // Проверяем, что входной параметр не null/undefined и является строкой
+  if (str === null || typeof str !== 'string') {
+      return false;
+  }
+  
+  // Удаляем пробелы в начале и конце строки
+  str = str.trim();
+  
+  // Проверяем, что строка не пустая
+  if (str.length === 0) {
+      return false;
+  }
+  
+  // Проверяем, является ли строка числом
+  return !isNaN(str) && !isNaN(parseFloat(str));
 }
 
-console.log(isNumeric("123")) // Ожидаемый результат: true
-console.log(isNumeric("12.3")) // Ожидаемый результат: true
-console.log(isNumeric("123abc")) // Ожидаемый результат: false
-console.log(isNumeric("abc")) // Ожидаемый результат: false
-console.log(isNumeric(" ")) // Ожидаемый результат: false
+console.log(isNumeric("123"))     // true
+console.log(isNumeric("12.3"))    // true
+console.log(isNumeric("123abc"))  // false
+console.log(isNumeric("abc"))     // false
+console.log(isNumeric(" "))       // false
